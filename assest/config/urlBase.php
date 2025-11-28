6a2ac04d1c8e592ee322beea6d6fb953e0e1a0af
desarrollovolcan <eisla@fvolcan.cl>
2025-11-28 00:13:12 -0300

    Segunda tanda de cambios

--- assest/config/urlBase.php
+++ assest/config/urlBase.php
@@ -1,60 +1,55 @@
 <?php 
 
-
 ?>
 
-    <!-- Vendor JS -->
-	<script src="../../api/cryptioadmin10/html/main/js/vendors.min.js"></script>
-    
-    <script src="../../api/cryptioadmin10/html/assets/vendor_components/c3/d3.min.js"></script>
-    <script src="../../api/cryptioadmin10/html/assets/vendor_components/c3/c3.min.js"></script>
+    <!-- Core JS -->
+    <script src="../../assest/js/jquery.min.js"></script>
+    <script src="../../api/bootstrap/js/bootstrap.bundle.min.js"></script>
 
-	<script src="../../api/cryptioadmin10/html/assets/vendor_components/bootstrap-select/dist/js/bootstrap-select.js"></script>
-	<script src="../../api/cryptioadmin10/html/assets/vendor_components/bootstrap-tagsinput/dist/bootstrap-tagsinput.js"></script>
-	<script src="../../api/cryptioadmin10/html/assets/vendor_components/bootstrap-touchspin/dist/jquery.bootstrap-touchspin.min.js"></script>
+    <!-- DataTables full bundle -->
+    <script src="../../api/DataTables/datatables.min.js"></script>
 
-	<script src="../../api/cryptioadmin10/html//assets/vendor_components/select2/dist/js/select2.full.js"></script>
-    <script src="../../api/cryptioadmin10/html/assets/vendor_components/datatable/datatables.min.js"></script>
-	<script src="../../api/cryptioadmin10/html/assets/vendor_components/jquery-steps-master/build/jquery.steps.js"></script>
+    <!-- DataTables export deps -->
+    <script src="../../api/DataTables/JSZip-2.5.0/jszip.min.js"></script>
+    <script src="../../api/DataTables/pdfmake-0.1.36/pdfmake.min.js"></script>
+    <script src="../../api/DataTables/pdfmake-0.1.36/vfs_fonts.js"></script>
 
-	
-	<script src="../../api/cryptioadmin10/html/assets/vendor_components/raphael/raphael.min.js"></script>
-	<script src="../../api/cryptioadmin10/html/assets/vendor_components/morris.js/morris.min.js"></script>  
+    <!-- Select2 -->
+    <script src="../../api/select2/js/select2.full.min.js"></script>
 
-    <script src="../../api/cryptioadmin10/html/assets/vendor_components/sweetalert/sweetalert.min.js"></script>
-    <script src="../../api/cryptioadmin10/html/assets/vendor_components/sweetalert/jquery.sweet-alert.custom.js"></script>
-   
-    <!--input mask-->
-	<script src="../../api/cryptioadmin10/html/assets/vendor_plugins/input-mask/jquery.inputmask.js"></script>
-	<script src="../../api/cryptioadmin10/html/assets/vendor_plugins/input-mask/jquery.inputmask.date.extensions.js"></script>
-	<script src="../../api/cryptioadmin10/html/assets/vendor_plugins/input-mask/jquery.inputmask.extensions.js"></script>
-    
-    <!--calendario timeer color--->
-	<script src="../../api/cryptioadmin10/html/assets/vendor_components/moment/min/moment.min.js"></script>
-	<script src="../../api/cryptioadmin10/html/assets/vendor_components/bootstrap-daterangepicker/daterangepicker.js"></script>
-	<script src="../../api/cryptioadmin10/html/assets/vendor_components/bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js"></script>
-	<script src="../../api/cryptioadmin10/html/assets/vendor_components/bootstrap-colorpicker/dist/js/bootstrap-colorpicker.min.js"></script>
-	<script src="../../api/cryptioadmin10/html/assets/vendor_plugins/timepicker/bootstrap-timepicker.min.js"></script>
-    
-    <!--check box radiobutton-->
-	<script src="../../api/cryptioadmin10/html/assets/vendor_plugins/iCheck/icheck.min.js"></script>
-	
-	<!-- Crypto Tokenizer Admin App -->
-	<script src="../../api/cryptioadmin10/html/main/js/template.js"></script>
-    <script src="../../api/cryptioadmin10/html/main/js/demo.js"></script>
-    <script src="../../api/cryptioadmin10/html/main/js/pages/c3-bar-pie.js"></script>
-	<script src="../../api/cryptioadmin10/html/main/js/pages/advanced-form-element.js"></script>   
-	<script src="../../api/cryptioadmin10/html/main/js/pages/steps.js"></script>   
-
-	<!--Api Datatables-->
-	<script type="text/javascript" src="../../api/DataTables/datatables.min.js"></script>	
-	<script src="../../api/DataTables/data-table.js"></script>
-	<script src="../../api/DataTables/Plugins-master/api/sum().js"></script>
-	<script src="../../api/cryptioadmin10/html/assets/vendor_components/jquery-toast-plugin-master/src/jquery.toast.js"></script>
-	
+    <!-- Configuración específica de tablas -->
+    <script src="../../api/DataTables/data-table.js"></script>
 
 
 
-	<!-- JS personalizados -->
-    
-    
+    <script>
+      // Menú desplegable tipo treeview (módulo / submódulo)
+      document.addEventListener('DOMContentLoaded', function () {
+        if (!window.jQuery) return;
+        var $ = window.jQuery;
+
+        // marcar li que tienen submenu
+        $('.sidebar-menu li.treeview').each(function () {
+          var $li = $(this);
+          var $submenu = $li.children('.treeview-menu');
+          if ($submenu.length) {
+            // si algún hijo está activo, abrir por defecto
+            if ($submenu.find('li.active').length) {
+              $li.addClass('menu-open');
+              $submenu.show();
+            }
+          }
+        });
+
+        // click en el enlace principal despliega / oculta
+        $('.sidebar-menu').on('click', 'li.treeview > a', function (e) {
+          var $li = $(this).parent();
+          var $submenu = $li.children('.treeview-menu');
+          if ($submenu.length) {
+            e.preventDefault();
+            $li.toggleClass('menu-open');
+            $submenu.slideToggle(150);
+          }
+        });
+      });
+    </script>
--- assest/config/urlHead.php
+++ assest/config/urlHead.php
@@ -1,6 +1,12 @@
 <?php ?>
 <!--        icono de  las pestaña  del navegado    -->
 <link rel="icon" href="../../assest/img/favicon.png">
+<!-- Fuentes modernas y Material Icons -->
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;500;600;700&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
 <!--Estilo base-->
 <link rel="stylesheet" type="text/css" HREF="../../assest/css/reset.css" />
 <link rel="stylesheet" type="text/css" HREF="../../assest/css/style.css" />
@@ -26,4 +32,6 @@
 
 
 
+
+<link rel="stylesheet" type="text/css" href="../../assest/css/theme-velzon.css" />
 <link rel="stylesheet" type="text/css" href="../../assest/css/layout-lite.css" />
