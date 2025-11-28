4e4d9cee0ac099c4740c18bc230d96e41b674cea
desarrollovolcan <eisla@fvolcan.cl>
2025-11-27 22:50:22 -0300

    Carga inicial

---
+++ assest/config/urlHead.php
@@ -0,0 +1,29 @@
+<?php ?>
+<!--        icono de  las pestaña  del navegado    -->
+<link rel="icon" href="../../assest/img/favicon.png">
+<!--Estilo base-->
+<link rel="stylesheet" type="text/css" HREF="../../assest/css/reset.css" />
+<link rel="stylesheet" type="text/css" HREF="../../assest/css/style.css" />
+
+
+<!--     bootstrap  -->        
+<link rel="stylesheet" href="../../api/bootstrap/css/bootstrap.css" />
+<link rel="stylesheet" href="../../api/bootstrap/css/bootstrap.min.css" />
+
+<!-- Plantilla Estilo-->
+<!-- Vendors Style-->
+<!-- Style-->
+<link rel="stylesheet" type="text/css" href="../../assest/css/theme-corporativo.css" />
+<!--Api Datatables-->
+<link rel="stylesheet" type="text/css" href="../../api/DataTables/datatables.min.css"/> 
+
+<!--Otros-->
+<!--JS -->
+<script src="../../assest/js/jquery.min.js"></script>    
+<!--sweetalert-->
+<script src="../../assest/js/sweetalert2@11.js"></script>
+
+
+
+
+<link rel="stylesheet" type="text/css" href="../../assest/css/layout-lite.css" />
---
+++ index.php
@@ -0,0 +1,102 @@
+<html lang="es-Ep">
+
+<head>
+    <title>Sistemas</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+    <link rel="stylesheet" type="text/css" href="assest/css/style.css" />
+    <link rel="stylesheet" type="text/css" HREF="assest/css/reset.css" />
+    <link rel="icon" href="assest/img/favicon.png">
+
+    <!-- Plantilla Estilo-->
+    <!-- Vendors Style-->
+
+    <link rel="stylesheet" href="api/cryptioadmin10/html/main/css/vendors_css.css">
+    <!-- Style-->
+    <link rel="stylesheet" href="api/cryptioadmin10/html/main/css/style.css">
+    <link rel="stylesheet" href="api/cryptioadmin10/html/main/css/skin_color.css">
+
+    <style>
+        body, html {
+            height: 100%;
+            margin: 0;
+        }
+
+        .split {
+            height: 100%;
+            width: 50%;
+            position: fixed;
+            top: 0;
+            overflow-x: hidden;
+        }
+
+        .right {
+            width: 30%;
+            background-color: transparent;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            right: 0px;
+
+            
+        }
+
+        .left {
+            background-image: url('./assest/img/fondo.jpg'); /* Imagen de fondo de ejemplo */
+            width: 100%;
+            /*background: red;*/
+            background-position: center;
+            background-repeat: no-repeat;
+            background-size: cover;
+
+           
+        }
+
+
+    </style>
+
+</head>
+
+<body class="hold-transition theme-primary bg-gradient-primary">
+
+
+
+
+    <div class="split left">
+
+
+
+    <div class="split right">
+                        <div class="bg-white-10 rounded5" style="background-color: white; width: 100%;">
+
+                            <div class="content-top-agile p-10 pb-0">
+                                <h2 class="text-primary">SELECCION INTRANET</h2>
+                            </div>
+                            <div class="p-30" style="width: 70%; margin: auto;">
+                                <div class="col-xxl-12 col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 col-xs-12">
+                                    <div class="form-group">
+                                        <a href="./interno.php">
+                                            <button type="button" class="btn bg-gradient-primary btn-block" id="ENTRAR" name="ENTRAR">Interno</button>
+                                        </a>
+                                    </div>
+                                </div>
+                                <div class="col-xxl-12 col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 col-xs-12">
+                                    <div class="form-group">
+                                        <a href="./estadistica/">
+                                            <button type="button" class="btn bg-gradient-secondary btn-block " id="ENTRAR" name="ENTRAR">Productor</button>
+                                        </a>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+    </div>
+
+
+    </div>
+
+   
+
+</body>
+</html>
